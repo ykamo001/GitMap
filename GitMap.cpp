10dbf1ebca86fc238b0d685b6de003d3aa111048
Yash Kamothi <ykamo001@hammer.cs.ucr.edu>
2015-05-19 21:38:44 -0700

    able to take in branch names

--- GitMap.cpp
+++ GitMap.cpp
@@ -17,22 +17,109 @@
 
 using namespace std;
 using namespace boost;
+void execfunction(char **argv, string command)
+{
+	int pid = fork();
+	if(-1 == pid)
+	{
+		perror("There was an error with fork(). ");
+	}
+	else if(pid == 0)
+	{
+		if(-1 == execvp(command.c_str(), argv))
+		{
+			perror("There was an error with execvp() ");
+			exit(1);
+		}
+		_exit(0);
+	}
+	else
+	{
+		int status;
+		wait(&status);
+		if(-1 == status)
+		{
+			perror("There was an error with wait(). ");
+		}
+	}
+}
 
 int main()
 {
 	int savestdout;
+	if(-1 == (savestdout = dup(1)))
+	{
+		perror("There was an error with dup(). ");
+	}
 	if(-1 == close(1))
 	{
 		perror("There was an error with close(). ");
 	}
 	vector<string> branches;
 	//call execvp to run git branch
+	string file_name = "tempfile";
+	int write_to;
+	char **argv = new char*[3];
+	argv[0] = const_cast<char*>("git");
+	argv[1] = const_cast<char*>("branch");
+	argv[2] = 0;
+	if(-1 == (write_to = open(file_name.c_str() , O_CREAT | O_WRONLY | O_APPEND | O_TRUNC, S_IRUSR | S_IWUSR)))
+	{
+		perror("There was an error with open(). ");
+		exit(1);
+	}
+	execfunction(argv, "git");
+	delete []argv;
+	if(-1 == close(write_to))
+	{
+		perror("There was an error with close(). ");
+	}
+	if(-1 == dup2(savestdout, 1))
+	{
+		perror("There was an error with dup2(). ");
+	}
+	int read_from;
+	if(-1 == (read_from = open(file_name.c_str(), O_RDONLY)))
+	{
+		perror("There was an error with open(). ");
+	}
+	int size;
+	char c[BUFSIZ];
+	if(-1 == (size = read(read_from, &c, BUFSIZ))) 
+	{
+		perror("There was an error with read(). ");
+	}
+	while(size > 0)
+	{
+		branches.push_back(string(c));
+		if(-1 == (size = read(read_from, &c, BUFSIZ))) 
+		{
+			perror("There was an error with read(). ");
+		}
+	}
+	if(-1 == close(read_from))
+	{
+		perror("There was an error with close(). ");
+	}
 	//take all branch names and put into vector
 	//for each branch name, call execvp git checkout branchname
+	for(unsigned int i = 0; i < branches.size(); ++i)
+	{
+		cout << branches.at(i);
+		if(i < branches.size()-1)
+		{
+			cout << endl;
+		}
+	}
+	argv = new char*[3];
+	argv[0] = const_cast<char*>("rm");
+	argv[1] = const_cast<char*>(file_name.c_str());
+	argv[2] = 0;
+	execfunction(argv, "rm");
+	delete []argv;
 	//then open/create a file called branchname for whatever branch we are on
 	//then call execvp git log to have all the output go to file
 	//close file
 	//reopen stdout
-	int pid;
 	return 0;
 }
