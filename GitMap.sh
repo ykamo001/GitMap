5a4be80d6b5175a587c9f953f4e7a5a112ca1e8e
Yash Kamothi <ykamo001@hammer.cs.ucr.edu>
2015-06-07 16:47:28 -0700

    started loop to go to each branch

--- GitMap.sh
+++ GitMap.sh
@@ -3,6 +3,7 @@
 #make temporary files that will assure us that no file of that name exists
 temp_file="$(mktemp)"
 temp_file2="$(mktemp)"
+temp_file3="$(mktemp)"
 
 #create file to get all branch names
 git branch > $temp_file	
@@ -16,8 +17,17 @@ branches=`wc -l $temp_file | awk '{print $1;}'`
 
 echo $branches
 echo $currBranch
+#this will let us traverse all our branches as if they were in a vector of string
+count=1
+while [ $count -le $branches ]
+do
+	sed -n ''$count'p' < $temp_file | awk '{print $1;}' > $temp_file3
+	cat $temp_file3
+	((count++))
+done
 rm $temp_file
 rm $temp_file2
+rm $temp_file3
 #cat masterBranch | sed "s/commit.*//g" | sed "s/Merge:.*//g" | sed 's/<.*>//g' | sed '/^\s*$/d'
 
 echo "Finished Script"
