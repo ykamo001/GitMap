624279bb441f96dc2640df0e7d6ea9dcea86f074
Yash Kamothi <ykamo001@hammer.cs.ucr.edu>
2015-06-07 14:41:45 -0700

    converting program to script

--- GitMap.sh
+++ GitMap.sh
@@ -1,5 +1,13 @@
 #! /bin/bash/
 
-cat masterBranch | sed "s/commit.*//g" | sed "s/Merge:.*//g" | sed 's/<.*>//g' | sed '/^\s*$/d'
+temp_file="$(mktemp)"
+git branch > $temp_file
+cat $temp_file
+#currGit=`cat $temp_file | grep '*'`
+#branches=`wc -l $temp_file`
+#echo $branches
+#echo $currGit
+rm $temp_file
+#cat masterBranch | sed "s/commit.*//g" | sed "s/Merge:.*//g" | sed 's/<.*>//g' | sed '/^\s*$/d'
 
 echo "Finished Script"
